3320de06d8c3e851b693fa4e2c24d4b5190b35d3
Tanmay Pradhan <tanmaypradhan1990@gmail.com>
2022-10-01 15:45:57 +0200

    Player Control, Bottle Physics
    Added Scene, Player Control, Bottle Control

---
+++ Assets/Scripts/BottleController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottleController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float forceValue=20f;
+    public float gravityMod=1;
+
+    private Vector3 forceToAdd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityMod;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Throw();
+        }
+    }
+
+    void Throw()
+    {
+        forceToAdd = new Vector3(0.35f, 1, 0);
+        rb.AddForce(forceToAdd * forceValue, ForceMode.Impulse);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            
+            if(Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.transform);
+                GameObject.Destroy(hit.transform.gameObject);
+            }
+        }
+    }
+
+   
+}
